c153e434d47cb499c650115122f30d7bb9ec6054
juandavidvargasrobayo <vargasrobayojuandavid43@gmail.com>
2026-06-22 19:15:55 -0500

    se construyo la pagina home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './home.css'
+ 
 })
 export default class Home {}
